07d566f09ce26533459992bf81ff94ec8bc1aeae
danielkalari <danielkalari76@gmail.com>
2024-10-03 13:06:17 +0000

    Initial commit

---
+++ 0-from_numpy.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import pandas as pd
+
+def from_numpy(array):
+    """Creates a DataFrame from a np.ndarray with columns labeled A-Z"""
+    num_cols = array.shape[1]
+    columns = [chr(i) for i in range(65, 65 + num_cols)]
+    df = pd.DataFrame(array, columns=columns)
+    return df
