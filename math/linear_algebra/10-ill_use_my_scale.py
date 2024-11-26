630cec6bd76ce111ee2342a5fbf2eef1a381c90d
Danjel003 <danielkalari76@gmail.com>
2024-11-26 20:05:46 +0000

    Ill use my scale

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Write a function that calculates the shape of a numpy """
+
+
+def np_shape(matrix):
+    """ A function that calculates the shape of a numpy """
+
+    return matrix.shape
