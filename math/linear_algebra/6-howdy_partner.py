14b834c72c7833567a5c448faecfa5b6fa40470a
Danjel003 <danielkalari76@gmail.com>
2024-11-26 16:41:10 +0000

    Howdy partner

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Write a function that concatenates two arrays """
+
+
+def cat_arrays(arr1, arr2):
+    """ A function that concatenates two arrays """
+
+    result = []
+    result.append(arr1 + arr2)
+    return result
