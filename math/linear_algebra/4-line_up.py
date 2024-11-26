a54c6a6e2985826e7adb4fe4f6335d371cf7c415
Danjel003 <danielkalari76@gmail.com>
2024-11-26 16:19:48 +0000

    Sum of two arrays

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-""" Write a function def add_arrays(arr1, arr2): that adds two arrays element-wise """
+""" Write a function that adds two arrays element-wise """
 
 
 def add_arrays(arr1, arr2):
     """ A function that adds two arrays element-wise """
     if len(arr1) != len(arr2):
         return None
-    
+
     result = []
     for i in range(len(arr1)):
         result.append(arr1[i] + arr2[i])
-    
+
     return result
