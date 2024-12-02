fde5ebf584bd7a51737b9204ac5005393fecec16
Danjel003 <danielkalari76@gmail.com>
2024-12-02 17:55:44 +0000

    Poly derivative

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 """ Derivative """
 
-
 def poly_derivative(poly):
+"""    Kontrolli nëse është një listë e vlefshme """
     if not isinstance(poly, list):
         return None
-    """Nëse polinomi është një konstante, derivati është 0"""
+
+    """Nëse polinomi ka vetëm një element, derivati është 0 (për një konstante)"""
     if len(poly) <= 1:
         return [0]
 
-    """ Llogaritja e derivatit duke përdorur një listë të re"""
-    return [i * poly[i] for i in range(1, len(poly))]
+    """ """
+    return [poly[i] * i for i in range(1, len(poly))]
