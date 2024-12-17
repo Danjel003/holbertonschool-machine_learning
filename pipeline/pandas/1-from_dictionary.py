faeeebb022fd07abf96eeac94171df0c501929ca
Danjel003 <danielkalari76@gmail.com>
2024-12-17 14:39:19 +0000

    Tasks

--- pipeline/pandas/1-from_dictionary.py
+++ pipeline/pandas/1-from_dictionary.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 
 d = {
-    'First': [0, 0.5, 1, 1.5], 
+    'First': [0, 0.5, 1, 1.5],
     'Second': ['one', 'two', 'three', 'four']
     }
 df = pd.DataFrame(d, index=list('ABCD'))
