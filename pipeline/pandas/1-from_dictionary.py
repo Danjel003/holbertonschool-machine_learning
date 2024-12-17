9dcc882e4722a4bb4d234a16a4d6f97307bce031
Danjel003 <danielkalari76@gmail.com>
2024-12-17 14:38:25 +0000

    Tasks

--- pipeline/pandas/1-from_dictionary.py
+++ pipeline/pandas/1-from_dictionary.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
-
-
+"""
+Task 1: script that creates a pd.DataFrame from a dictionary:
+"""
 import pandas as pd
 
-# Krijimi i fjalorit
-data = {
-    "First": [0.0, 0.5, 1.0, 1.5],
-    "Second": ["one", "two", "three", "four"]
-}
 
-# Krijimi i DataFrame
-df = pd.DataFrame(data, index=["A", "B", "C", "D"])
+d = {
+    'First': [0, 0.5, 1, 1.5], 
+    'Second': ['one', 'two', 'three', 'four']
+    }
+df = pd.DataFrame(d, index=list('ABCD'))
