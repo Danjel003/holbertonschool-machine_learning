00e863ca3d130f848043f80c9ae497c8a70852ad
Danjel003 <danielkalari76@gmail.com>
2024-12-17 14:14:58 +0000

    Concat

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python3
+"""
+Task 11: Concat
+"""
 
 
-import pandas as pd
-from_file = __import__('2-from_file').from_file
+def concat(df1, df2):
+    """ Concat"""
+    df1.set_index('Timestamp', inplace=True)
+    df2.set_index('Timestamp', inplace=True)
 
-df1 = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
-df2 = from_file('bitstampUSD_1-min_data_2012-01-01_to_2020-04-22.csv', ',')
+    # Filter df2 to only include timestamps up to and including 1417411920
+    df2 = df2.loc[:1417411920]
 
-# Set the index to the Timestamp column
-df1.set_index('Timestamp', inplace=True)
-df2.set_index('Timestamp', inplace=True)
+    # Concatenate the DataFrames
+    df = pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])
 
-# Filter df2 to only include timestamps up to and including 1417411920
-df2 = df2.loc[:1417411920]
-
-# Concatenate the DataFrames
-df = pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])
-
-print(df)
+    return df
