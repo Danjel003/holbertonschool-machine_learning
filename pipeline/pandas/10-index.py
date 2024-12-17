9851033ec607cccb16aacde3ae57cdb6f0f871bf
Danjel003 <danielkalari76@gmail.com>
2024-12-17 14:10:00 +0000

    Indexing

--- pipeline/pandas/10-index.py
+++ pipeline/pandas/10-index.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
+"""
+Task 10: Indexing
+"""
 
 
-import pandas as pd
-from_file = __import__('2-from_file').from_file
-
-df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
-
-# Set the index to the Timestamp column
-df.set_index('Timestamp', inplace=True)
-
-print(df.tail())
+def index(df):
+    """Indexing"""
+    df.set_index('Timestamp', inplace=True)
+    return df
