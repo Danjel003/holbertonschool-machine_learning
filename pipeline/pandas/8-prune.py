5f67af41558a032f663562e2d33fdbed5b5b3ad6
Danjel003 <danielkalari76@gmail.com>
2024-12-17 14:03:36 +0000

    Prune

--- pipeline/pandas/8-prune.py
+++ pipeline/pandas/8-prune.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
+"""
+Task 8: Removes any entries where Close has NaN values.
+"""
 
 
-import pandas as pd
-from_file = __import__('2-from_file').from_file
-
-df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
-
-# Remove entries where Close is NaN
-df = df.dropna(subset=['Close'])
-
-print(df.head())
+def prune(df):
+    """ Removes any entries where Close has NaN values"""
+    df = df.dropna(subset=['Close'])
+    return df
