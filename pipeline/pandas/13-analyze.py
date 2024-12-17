f030deb73f77c4269f00be5dc61b38b5b55585ec
Danjel003 <danielkalari76@gmail.com>
2024-12-17 14:28:01 +0000

    Analyze

--- pipeline/pandas/13-analyze.py
+++ pipeline/pandas/13-analyze.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
-
-
+"""
+Task 13: Analyze
+"""
 import pandas as pd
-from_file = __import__('2-from_file').from_file
 
-# Load the data from the CSV file
-df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 
-# Calculate descriptive statistics for all columns except Timestamp
-stats = df.describe()
-
-# Display the resulting statistics
-print(stats)
+def analyze(df):
+    # Calculate descriptive statistics for all columns except Timestamp
+    stats = df.drop(columns=['Timestamp']).describe()
+    return status
