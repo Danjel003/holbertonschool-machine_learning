03a79c44260f6a5402affe679e5d77b3508d0ac0
Danjel003 <danielkalari76@gmail.com>
2024-12-17 14:21:57 +0000

    Hierarchy

--- pipeline/pandas/12-hierarchy.py
+++ pipeline/pandas/12-hierarchy.py
@@ -1,25 +1,24 @@
 #!/usr/bin/env python3
-
-
+"""
+Task 12: Hierarchy
+"""
 import pandas as pd
 from_file = __import__('2-from_file').from_file
 
-# Load the data from the CSV files
-df1 = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
-df2 = from_file('bitstampUSD_1-min_data_2012-01-01_to_2020-04-22.csv', ',')
 
-# Filter the DataFrames for the specified timestamps
-df1_filtered = df1[(df1.index >= 1417411980) & (df1.index <= 1417417980)]
-df2_filtered = df2[(df2.index >= 1417411980) & (df2.index <= 1417417980)]
+def hierarchy(df1, df2):
+    # Filter the DataFrames for the specified timestamps
+    df1_filtered = df1[(df1.index >= 1417411980) & (df1.index <= 1417417980)]
+    df2_filtered = df2[(df2.index >= 1417411980) & (df2.index <= 1417417980)]
 
-# Concatenate the DataFrames and add keys
-df = pd.concat([df2_filtered, df1_filtered], keys=['bitstamp', 'coinbase'])
+    # Concatenate the DataFrames and add keys
+    df = pd.concat([df2_filtered, df1_filtered], keys=['bitstamp', 'coinbase'])
 
-# Reorganize the MultiIndex to have timestamp as the first level
-df.index = df.index.reorder_levels(['Timestamp', 'key'])
+    # Reorganize the MultiIndex to have timestamp as the first level
+    df.index = df.index.reorder_levels(['Timestamp', 'key'])
 
-# Sort the DataFrame by index (which now includes timestamp first)
-df = df.sort_index(level='Timestamp')
+    # Sort the DataFrame by index (which now includes timestamp first)
+    df = df.sort_index(level='Timestamp')
 
-# Display the resulting DataFrame
-print(df)
+    # Display the resulting DataFrame
+    return df
