5d4335ae0ec8306561d346848d98fcf2a2b24109
Danjel003 <danielkalari76@gmail.com>
2024-12-17 13:59:02 +0000

    Flip it and Switch it

--- pipeline/pandas/6-flip_switch.py
+++ pipeline/pandas/6-flip_switch.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
+"""
+Task 6: sort and tranpose a pandas dataframe
+"""
 
 
-import pandas as pd
-from_file = __import__('2-from_file').from_file
-
-df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
-
-# Transpose the DataFrame and sort in reverse chronological order
-df = df.T.sort_index(ascending=False)
-
-print(df.tail(8))
+def flip_switch(df):
+    """ sort and tranpose a pandas dataframe """
+    df = df.sort_index(ascending=False).T
+    return df
