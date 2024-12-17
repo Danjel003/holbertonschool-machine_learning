248f40f7f709548cf0c1802e5be497a4e30107ff
Danjel003 <danielkalari76@gmail.com>
2024-12-17 14:01:59 +0000

    Sort

--- pipeline/pandas/7-high.py
+++ pipeline/pandas/7-high.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
+"""
+Task 7: Sorts pandas dataframe by a column
+"""
 
 
-import pandas as pd
-from_file = __import__('2-from_file').from_file
-
-df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
-
-# Sort the DataFrame by the High price in descending order
-df = df.sort_values(by='High', ascending=False)
-
-print(df.head())
+def high(df):
+    """ Sorts pandas dataframe by a column """
+    df = df.sort_values(by='High', ascending=False)
+    return df
