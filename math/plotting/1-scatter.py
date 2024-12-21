91da370125a4f5356cbc5f19ea9df2f9fa57a452
Danjel003 <danielkalari76@gmail.com>
2024-12-21 20:55:26 +0000

    Scatter

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
+"""
+Task 1
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def scatter():
-
+    """
+    """
     mean = [69, 0]
     cov = [[15, 8], [8, 15]]
     np.random.seed(5)
@@ -12,6 +17,7 @@ def scatter():
     y += 180
     plt.figure(figsize=(6.4, 4.8))
 
+    # your code here
     plt.xlabel('Height (in)')
     plt.ylabel('Weight (lbs)')
     plt.title("Men's Height vs Weight")
