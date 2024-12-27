d86ea817c05bb9217cb7c1b8ca685480a66c6353
Danjel003 <103456642+Danjel003@users.noreply.github.com>
2024-12-27 21:46:24 +0100

    alculates the shape of a matrix:

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
+""" A script that calculates the shape of a matrix """
 
-matrix_transpose = __import__('3-flip_me_over').matrix_transpose
 
-mat1 = [[1, 2], [3, 4]]
-print(mat1)
-print(matrix_transpose(mat1))
-mat2 = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15],
-        [16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]
-print(mat2)
-print(matrix_transpose(mat2))
+def matrix_shape(matrix):
+    """ A function  that calculates the shape of a matrix"""
+    matrix_shape = []
+    while (type(matrix) is list):
+        matrix_shape.append(len(matrix))
+        matrix = matrix[0]
+    return matrix_shape
